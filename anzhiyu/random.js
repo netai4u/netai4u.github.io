ca9fc58c5a77cd31d2b4b6079246e6309e9c77f8
netai4u <26784584@qq.com>
2025-05-13 16:41:43 +0800

    Site updated: 2025-05-13 16:41:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/13/这是一篇博文/","2025/05/13/我自己的测试/","2025/05/13/first-test-blog/","2025/05/07/hello-world/"];function toRandomPost(){
+var posts=["2025/05/13/这是一篇博文/","2025/05/13/测试YouTube/","2025/05/13/我自己的测试/","2025/05/13/first-test-blog/","2025/05/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
