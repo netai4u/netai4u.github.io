f62d621c15270abbb8be93b861c07a8efdfece90
netai4u <26784584@qq.com>
2025-05-16 15:28:47 +0800

    Site updated: 2025-05-16 15:28:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/13/这是一篇博文/","2025/05/13/测试YouTube/","2025/05/13/我自己的测试/","2025/05/13/first-test-blog/","2025/05/07/hello-world/"];function toRandomPost(){
+var posts=["2025/05/13/开通博客的初衷/","2025/05/13/Web前端就业前景分析/","2025/05/13/今年前端开发行业的就业行情如何呢？/","2025/05/13/面试必问，刷完就能通过/","2025/05/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
